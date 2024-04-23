4c25e1cce525fecb9fe99d9043c5f2070cb7b4cf
Aixa AndradeHernandez <s437576@NucleusC035.cm.cluster>
2024-04-23 11:29:23 -0500

    added AE expt

---
+++ heart_data/run_models/Healthy_human_heart/log_transformed_3000hvggenes/AE/scripts/sbatch_run_AE.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=AE
+#SBATCH --partition=GPUv100s
+#SBATCH --time=4-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH -o job_%j.out
+#SBATCH -e job_%j.err
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+module load cuda118
+module load cuda118/toolkit/11.8.0
+module load parallel
+module load python/3.7.x-anaconda
+source activate /archive/bioinformatics/DLLab/shared/CondaEnvironments/Aixa_ARMED_2
+
+python run_AE_allfolds.py

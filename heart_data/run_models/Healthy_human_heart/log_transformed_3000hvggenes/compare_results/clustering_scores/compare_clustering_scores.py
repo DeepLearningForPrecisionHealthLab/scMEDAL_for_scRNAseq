b533c847753481a42728a69ec055bba362f6e372
Aixa AndradeHernandez <s437576@NucleusA250.cm.cluster>
2024-11-06 15:18:21 -0600

    updated compare_clustering_scores

--- heart_data/run_models/Healthy_human_heart/log_transformed_3000hvggenes/compare_results/clustering_scores/compare_clustering_scores.py
+++ heart_data/run_models/Healthy_human_heart/log_transformed_3000hvggenes/compare_results/clustering_scores/compare_clustering_scores.py
@@ -1,8 +1,8 @@
 # Set expt in path2results.py
-# "expt_3_with_batch_norm_clusteringlosscurves_HP0_adjustment4_500epochs_linear_latentspace50" or expt=="expt3_batch_cf"
+
 import sys
 sys.path.append("/archive/bioinformatics/DLLab/AixaAndrade/src/ARMED_genomics_git/heart_data/run_models/Healthy_human_heart/log_transformed_3000hvggenes/compare_results")
-from path2results import run_names_dict,results_path_dict,run_names_dict, glob_like,compare_models_path ,get_pca
+from path2results import run_names_dict,results_path_dict,run_names_dict,compare_models_path
 import pandas as pd
 import glob
 import os
@@ -10,7 +10,8 @@ import numpy as np
 import scipy.stats as stats
 
 sys.path.append("/archive/bioinformatics/DLLab/AixaAndrade/src/ARMED_genomics_git/utils")
-from compare_results_utils import preprocess_results_model_pca_format,process_single_model_format,calculate_and_append_ci,glob_like
+# from compare_results_utils import preprocess_results_model_pca_format,process_single_model_format,calculate_and_append_ci,glob_like
+from compare_results_utils import aggregate_paths,read_and_aggregate_scores,filter_min_max_silhouette_scores,process_all_results, process_confidence_intervals
 
 
 #Normally I run it with Aixa_ARMED_2.
@@ -20,32 +21,54 @@ from compare_results_utils import preprocess_results_model_pca_format,process_si
 
 # 1. Find all file paths that you want to compare for all models and store them in a dataframe
 # If you are developing use val dataset. If this is your final result use test dataset
-
-paths_dict = {}
-for model_name,path in results_path_dict.items(): 
-    print(model_name,path)
-    paths_dict[model_name]=glob_like(path, 'mean_scores_val_samplesize')
-
-# Build df with all results path
-# Initialize an empty DataFrame to store all the data
-df_all_paths = pd.DataFrame()
-
-# Iterate over the dictionary items
-for model_name, files in paths_dict.items():
-    # Extract the sample size from each file path
-    sample_size = [file.split("samplesize-")[-1].split(".csv")[0] for file in files]
-    # Create a DataFrame for the current batch of files
-    df = pd.DataFrame({"sample_size": sample_size, "path": files})
-    df['model_name'] = model_name  # Add a column for the model name
-    # Append the current DataFrame to the aggregated DataFrame
-    df_all_paths = df_all_paths.append(df, ignore_index=True)  # Ensure to reassign and use ignore_index=True
-
-# Now df_all contains all the data combined from the different files and models
-df_all_paths
+# dataset_type='val'
+dataset_type='test'
 
 
+# 1. Define how to name your output files
+out_name = os.path.join(compare_models_path, run_names_dict["run_name_all"])
+# Check if the directory exists, if not, create it
+if not os.path.exists(out_name):
+    os.makedirs(out_name)
 
-# 2. Define how to format your results depending on the model_confil.py settings
+print(f"Directory created or already exists: {out_name}")
+
+# 2. Get paths
+
+df_all_paths = aggregate_paths(results_path_dict, pattern = f'mean_scores_{dataset_type}_samplesize')
+print("\n\nmean scores paths",df_all_paths.head())
+
+
+df_all_paths_allscores = aggregate_paths(results_path_dict, pattern = f'all_scores_{dataset_type}_samplesize')
+print("\nall scores paths",df_all_paths_allscores)
+
+print("\nall scores paths cols",df_all_paths_allscores.columns )
+
+# 3. Read all scores
+df_allscores = read_and_aggregate_scores(df_all_paths_allscores)
+print("\ndf with all scores",df_allscores)
+df_allscores.to_csv(os.path.join(out_name, f"{dataset_type}_allscores.csv"))
+# for index, row in df_all_paths_allscores.iterrows():
+#    print("\n",row['model_name'], row['path'].split('/')[-1])
+# print("\n\n\ndf_allscores.columns",df_allscores.columns)
+# Getting rows with max and min silhouette score
+
+# 4. Filter min and max_sil scores from batch 
+for sample_size in np.unique(df_allscores["sample_size"]):
+    df_min_silhouette, df_max_silhouette = filter_min_max_silhouette_scores(df_allscores, batch_col="batch")
+    df_min_silhouette.to_csv(os.path.join(out_name, f"{dataset_type}_scores_{sample_size}_min_silhouette_batch.csv"))
+    df_max_silhouette.to_csv(os.path.join(out_name, f"{dataset_type}_scores_{sample_size}_max_silhouette_batch.csv"))
+
+
+    # Print the resulting DataFrames
+    print("DataFrame with minimum silhouette scores:")
+    print(df_min_silhouette)
+
+    print("\nDataFrame with maximum silhouette scores:")
+    print(df_max_silhouette)
+
+
+# 5. Define how to format your results depending on the model_confil.py settings
 # Edit this dictionary depending of how you want to process each data, 
 # if get_pca ==True for any model when you run the pipeline. You will need to use "preprocess_results_model_pca_format"
 # else use "process_single_model_format" or "" when you dont want to process it
@@ -57,76 +80,16 @@ models2process_dict = {"AE":"preprocess_results_model_pca_format",
                                         }
                                     
 
-# 3. Define how to name your output files
-out_name = os.path.join(compare_models_path, run_names_dict["run_name_all"])
-process_single_format = {}
-type_of_processing_dict = {"AE"}
-df_sample_size = {}
 
-# 4. Merge all your results into a single dataframe
-# for loop over sample_size and model_name
-for sample_size in np.unique(df_all_paths["sample_size"]):
-    print("sample size",sample_size)
-    model_path_dict={}
-    # df for single model format
-    df_smf = pd.DataFrame()
-    df_pcaf = pd.DataFrame()
-    count_i=0
-    for model_name in np.unique(df_all_paths["model_name"]):
 
-        # getting results file path
-        print(model_name)
-        file_paths = df_all_paths.loc[(df_all_paths["sample_size"]==sample_size)&(df_all_paths["model_name"]==model_name),"path"].values        
-        if len(file_paths)>0:
-            file_path = file_paths[0]
-            print(model_name,"file path",file_path)
-            model_path_dict[model_name]= file_path
-        # preprocessing models without pca results
-        if models2process_dict[model_name] == "process_single_model_format":
-            df_smf = df_smf.append(process_single_model_format(file_path=model_path_dict[model_name],model_name = model_name))
-            print("smf",df_smf)
-        # preprocessing models with pca results 
-        elif models2process_dict[model_name] == "preprocess_results_model_pca_format":
-            df_pcaf_i = preprocess_results_model_pca_format(model_path_dict[model_name], columns_to_drop = ['fold', 'sem_fold'])
-            df_pcaf_i = df_pcaf_i.rename(columns={"X_pca_val": "X_pca_val_"+str(count_i)})
-            df_pcaf = pd.concat([df_pcaf , df_pcaf_i ],axis=1) 
-            
-            count_i=+1
+df_sample_size = process_all_results(df_all_paths, models2process_dict, out_name, dataset_type)
 
-        else:
-            print(model_name, "not processed")
-        
-    df_all = pd.concat([df_pcaf , df_smf.T],axis=1).T   
-    ## Save model results dataframe for each sample-size
-    df_all.to_csv(out_name+sample_size+".csv")
-    print(df_all)
-    df_sample_size[sample_size]=df_all
 
 
 # 4. Get 95% CI
-df_ci = calculate_and_append_ci(df_all, dof=4)  # n=5, dof=n-1=4
 
-# Extract only mean and CI columns
-mean_ci_columns = [col for col in df_ci.columns if 'mean' in col[2] or 'CI' in col[2]]
-df_mean_ci = df_ci[mean_ci_columns]
+for sample_size, df_all in df_sample_size.items():
+    df_mean_ci_results = process_confidence_intervals(df_all, out_name, dataset_type, sample_size)
+    print("sample_size",sample_size,df_mean_ci_results)
 
-# Intercalate the mean and CI columns
-intercalated_columns = []
-for level_0 in df_ci.columns.levels[0]:
-    for level_1 in df_ci.columns.levels[1]:
-        mean_col = (level_0, level_1, 'mean')
-        ci_lower_col = (level_0, level_1, 'CI_lower')
-        ci_upper_col = (level_0, level_1, 'CI_upper')
-        if mean_col in df_mean_ci.columns:
-            intercalated_columns.append(mean_col)
-        if ci_lower_col in df_mean_ci.columns:
-            intercalated_columns.append(ci_lower_col)
-        if ci_upper_col in df_mean_ci.columns:
-            intercalated_columns.append(ci_upper_col)
 
-# Reorder the DataFrame columns based on intercalated columns
-df_mean_ci = df_mean_ci[intercalated_columns]
-# Save the df with 95% CI  to a CSV file
-df_mean_ci.to_csv(out_name+sample_size+"_95CI.csv")
-
-print(df_mean_ci)

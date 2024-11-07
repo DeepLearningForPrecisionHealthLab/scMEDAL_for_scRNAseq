55b976489e03793038f84ff26f83e63b1e100df7
Aixa AndradeHernandez <s437576@NucleusA250.cm.cluster>
2024-11-07 13:09:10 -0600

    added 5 fold cross val

---
+++ VanGallen_2019/preprocessing/5fold_cross_val/config_split_paths.py
@@ -0,0 +1,11 @@
+# Define general data paths to the all the simulation outputs
+
+data_path = "/archive/bioinformatics/DLLab/AixaAndrade/data/Genomic_data/VanGallen_2019"
+# Path data to split (path to the location of the data)
+
+data2split_foldername = "log_transformed_2916hvggenes"
+
+import os
+# folder in which the splits will be saved. They are saved inside of data2split_foldername. Please create your own paths to store data to avoid rewriting
+folder_splits = os.path.join(data2split_foldername,"splits")
+#folder_splits_unseen_pairs_odds = data2split_foldername  +"/unseen_splits_pairs_odds"

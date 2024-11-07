5b264876369574663f03d97a5a97bed6d59d7249
Aixa AndradeHernandez <s437576@NucleusA250.cm.cluster>
2024-11-07 12:35:11 -0600

    added scripts for genomaps

---
+++ ASD/run_models/compare_results/genomaps/sbatch_get_genomaps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=get_genomaps
+#SBATCH --partition=GPUp4
+#SBATCH --time=4-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+#SBATCH -o job_%j.out
+#SBATCH -e job_%j.err
+export CUDA_VISIBLE_DEVICES=0
+module load cuda118
+module load cuda118/toolkit/11.8.0
+module load parallel
+module load python/3.7.x-anaconda
+source activate /project/bioinformatics/DLLab/shared/CondaEnvironments/Aixa_genomap
+
+
+# python get_single_genomap_multibatch.py
+python pipeline_CMmultibatch_genomap_and_plot.py

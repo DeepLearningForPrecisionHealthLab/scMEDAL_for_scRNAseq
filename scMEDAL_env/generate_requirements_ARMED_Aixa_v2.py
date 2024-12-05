b38d1b0ede7f97013a2a268e2a3949349d4f6130
Aixa AndradeHernandez <s437576@NucleusA250.cm.cluster>
2024-12-05 10:54:54 -0600

    added env files

---
+++ scMEDAL_env/generate_requirements_ARMED_Aixa_v2.py
@@ -0,0 +1,117 @@
+import yaml
+
+# Initialize the environment dictionary
+environment = {
+    'name': 'Aixa_scDML',
+    'channels': ['conda-forge', 'defaults'],
+    'dependencies': []
+}
+
+# Function to add a package to dependencies
+def add_dependency(package_name, version, pip_package=False):
+    if pip_package:
+        # Check if 'pip' is already in dependencies
+        pip_index = next((index for (index, d) in enumerate(environment['dependencies']) if isinstance(d, dict) and 'pip' in d), None)
+        if pip_index is None:
+            # Add a new pip section
+            environment['dependencies'].append({'pip': [f"{package_name}=={version}"]})
+        else:
+            # Append to existing pip section
+            environment['dependencies'][pip_index]['pip'].append(f"{package_name}=={version}")
+    else:
+        environment['dependencies'].append(f"{package_name}={version}")
+
+# Add Python version
+import sys
+python_version = sys.version.split()[0]
+environment['dependencies'].append(f"python={python_version}")
+
+# Add packages
+try:
+    import numpy as np
+    add_dependency('numpy', np.__version__)
+except ImportError:
+    pass
+
+try:
+    import pandas as pd
+    add_dependency('pandas', pd.__version__)
+except ImportError:
+    pass
+
+try:
+    import tensorflow as tf
+    add_dependency('tensorflow', tf.__version__)
+except ImportError:
+    pass
+
+try:
+    import tensorflow_probability as tfp
+    add_dependency('tensorflow-probability', tfp.__version__)
+except ImportError:
+    pass
+
+try:
+    import tensorflow_addons as tfa
+    # Assuming tensorflow-addons is installed via pip
+    add_dependency('tensorflow-addons', tfa.__version__, pip_package=True)
+except ImportError:
+    pass
+
+try:
+    from tensorflow.keras import __version__ as keras_version
+    add_dependency('keras', keras_version)
+except ImportError:
+    pass
+
+try:
+    import scanpy as sc
+    add_dependency('scanpy', sc.__version__)
+except ImportError:
+    pass
+
+try:
+    import anndata
+    add_dependency('anndata', anndata.__version__)
+except ImportError:
+    pass
+
+try:
+    import scipy
+    add_dependency('scipy', scipy.__version__)
+except ImportError:
+    pass
+
+try:
+    import sklearn
+    add_dependency('scikit-learn', sklearn.__version__)
+except ImportError:
+    pass
+
+try:
+    import matplotlib
+    add_dependency('matplotlib', matplotlib.__version__)
+except ImportError:
+    pass
+
+try:
+    import seaborn as sns
+    add_dependency('seaborn', sns.__version__)
+except ImportError:
+    pass
+
+try:
+    import h5py
+    add_dependency('h5py', h5py.__version__)
+except ImportError:
+    pass
+
+try:
+    import yaml as pyyaml
+    add_dependency('pyyaml', pyyaml.__version__)
+except ImportError:
+    pass
+
+# Write the environment to a YAML file
+with open('run_models_env.yaml', 'w') as file:
+    yaml.dump(environment, file)
---
+++ scMEDAL_env/generate_requirements_Aixa_genomap.py
@@ -0,0 +1,111 @@
+import yaml
+
+# Initialize the environment dictionary
+environment = {
+    'name': 'Aixa_genomap',
+    'channels': ['conda-forge', 'defaults'],
+    'dependencies': []
+}
+
+# Function to add a package to dependencies
+def add_dependency(package_name, version, pip_package=False):
+    if pip_package:
+        # Check if 'pip' is already in dependencies
+        pip_index = next((index for (index, d) in enumerate(environment['dependencies']) if isinstance(d, dict) and 'pip' in d), None)
+        if pip_index is None:
+            # Add a new pip section
+            environment['dependencies'].append({'pip': [f"{package_name}=={version}"]})
+        else:
+            # Append to existing pip section
+            environment['dependencies'][pip_index]['pip'].append(f"{package_name}=={version}")
+    else:
+        environment['dependencies'].append(f"{package_name}={version}")
+
+# Add Python version
+import sys
+python_version = sys.version.split()[0]
+environment['dependencies'].append(f"python={python_version}")
+
+# Add packages
+try:
+    import numpy as np
+    add_dependency('numpy', np.__version__)
+except ImportError:
+    pass
+
+try:
+    import pandas as pd
+    add_dependency('pandas', pd.__version__)
+except ImportError:
+    pass
+
+
+
+try:
+    import scanpy as sc
+    add_dependency('scanpy', sc.__version__)
+except ImportError:
+    pass
+
+try:
+    import anndata
+    add_dependency('anndata', anndata.__version__)
+except ImportError:
+    pass
+
+try:
+    import scipy
+    add_dependency('scipy', scipy.__version__)
+except ImportError:
+    pass
+
+try:
+    import sklearn
+    add_dependency('scikit-learn', sklearn.__version__)
+except ImportError:
+    pass
+
+try:
+    import matplotlib
+    add_dependency('matplotlib', matplotlib.__version__)
+except ImportError:
+    pass
+
+try:
+    import seaborn as sns
+    add_dependency('seaborn', sns.__version__)
+except ImportError:
+    pass
+
+try:
+    import h5py
+    add_dependency('h5py', h5py.__version__)
+except ImportError:
+    pass
+
+try:
+    import yaml as pyyaml
+    add_dependency('pyyaml', pyyaml.__version__)
+except ImportError:
+    pass
+
+try:
+    import tensorflow as tf
+    add_dependency('tensorflow', tf.__version__)
+except ImportError:
+    pass
+
+try:
+    import genomap
+    #This is a manual check because genomap has no genomap.__version__
+    add_dependency('genomap', '1.3.6')
+except ImportError:
+    pass
+
+
+
+
+# Write the environment to a YAML file
+with open('genomaps_env.yaml', 'w') as file:
+    
+    yaml.dump(environment, file)
